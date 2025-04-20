9888a26a85bb6656d3ae4ed142f3ff0049863952
arunmishra136 <mishraarun136@gmail.com>
2025-04-21 01:10:51 +0530

    hore

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://mail-sender-backend-amkc.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://mail-sender-backend-amkc.onrender.com',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     }
+  //   }
+  // }
 })
